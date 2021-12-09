d06a1201d4e65c7951a4d5db6d7cc723f1fff64e
andrewKievUA <andrew1990mk@gmail.com>
2021-12-09 14:30:19 +0200

    add header  & navbar & ajust mapping

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import firebase from "firebase/compat";
 import "firebase/compat/firestore"
 import "firebase/compat/auth"
 import {useCollectionData} from "react-firebase-hooks/firestore";
-import {useAuthState} from "react-firebase-hooks/auth";
+//import {useAuthState} from "react-firebase-hooks/auth";
 
 const App = () => {
 
@@ -21,30 +21,69 @@ const App = () => {
     firebase.initializeApp(firebaseConfig)
 
     const firestore = firebase.firestore()
-    const messagesRef = firestore.collection("message")
+    //const messagesRef = firestore.collection("alarms")
 
     type Message={
-        age: number,
-        name: string
+        id: number,
+        time: string,
+        comment: string,
+        type:number
     }
 
-    const [message =[]] = useCollectionData<Message>(
-        firestore.collection("message")
-    )
 
+     const initialObj:Message = {
+        id: 0,
+        time: "",
+        comment: "",
+        type:0
+    }
+    const [message =[[initialObj]]] = useCollectionData<any>(
+        firestore.collection("alarms")
+    )
+  
+//console.log(  message);
 
 
     return (
-        <div>
-            {[message][0].map(el=>{
+    <div>
+       
+        <nav>
+            <div className="nav-wrapper">
+                <a href="#" className="brand-logo hide-on-med-and-down" >Logo</a>
+                <ul id="nav-mobile" className="right ">
+                    <li><a href="sass.html">Sass</a></li>
+                    <li><a href="badges.html">Components</a></li>
+                    <li><a href="collapsible.html">JavaScript</a></li>
+                </ul>
+            </div>
+        </nav>
+        <ul className="collection">
+        <table className="striped">
+            <thead>
+                <tr>
+                    <th>Событие</th>
+                    <th>Дата</th>
+                </tr>
+            </thead>
+             <tbody>
+            { message[0].addd? 
+            message[0].addd.map((el:Message)=>{
                 return(
-
-                    <div>{el.name} {el.age}</div>
-
+                    <tr key={el.id}>
+                    <td>{el.comment}</td>
+                    <td>{el.time.slice(11,-8)}</td>
+                  </tr>
+                       
                 )
-            })}
-            This is a app
-        </div>
+             })
+            :null}
+            </tbody>
+       </table>
+        </ul>
+
+
+        
+    </div>
     );
 };
 
